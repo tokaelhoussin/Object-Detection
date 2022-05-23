0d1ad625de01ee4ca4c5e3329690642a5f2f2a4e
ToQa-Mohamed <toukamohamed595@gmail.com>
2022-05-23 17:40:50 +0200

    Data preparation & Classifier

--- Vehicle_Detector.py
+++ Vehicle_Detector.py
@@ -208,3 +208,76 @@ non_car_features = extract_features(non_car_images,cspace=colorspace,orient=orie
 
 
 
+#Data Preparation
+# Preserve State to compare parameter tuning
+rand_state = np.random.randint(0, 100)
+
+# Create an array stack of feature vectors
+X = np.vstack((car_features, non_car_features)).astype(np.float64)
+# Fit a per-column scaler
+X_scaler = StandardScaler().fit(X)
+# Apply the scaler to X
+scaled_X = X_scaler.transform(X)
+
+# Define the labels vector
+y = np.hstack((np.ones(len(car_features)), np.zeros(len(non_car_features))))
+
+# Split up data into randomized training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    scaled_X, y, test_size=0.2, random_state=rand_state)
+
+print('Using spatial binning of:',spatial,'and', hist_bins,'histogram bins')
+print('Feature vector length:', len(X_train[0]))
+
+
+#Classifier
+# Use a linear SVC
+svc = LinearSVC()
+
+# Check the training time for the SVC
+t=time.time()
+svc.fit(X_train, y_train)
+t2 = time.time()
+
+print(round(t2-t, 2), 'Seconds to train SVC...')
+# Check the score of the SVC
+print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+
+# Prediction
+# Check the prediction time for a single sample
+t=time.time()
+n_predict = 10
+print('My SVC predicts:     ', svc.predict(X_test[0:n_predict]))
+print('For these',n_predict, 'labels: ', y_test[0:n_predict])
+t2 = time.time()
+print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
+
+#Data Saving to Pickle
+# Save data to pickle file
+dist_pickle = {}
+dist_pickle["svc"] = svc
+dist_pickle["scaler"] = X_scaler
+dist_pickle["orient"] = orient
+dist_pickle["pix_per_cell"] = pix_per_cell
+dist_pickle["cell_per_block"] = cell_per_block
+dist_pickle["spatial"] = spatial
+dist_pickle["hist_bins"] = hist_bins
+pickle.dump(dist_pickle, open("svc_pickle.p", 'wb') )
+
+print('Classifier parameters saved to file!')
+
+
+
+#Data Loading from Pickle
+print('Loading Classifier parameters...')
+dist_pickle = pickle.load( open("svc_pickle.p", "rb" ) )
+svc = dist_pickle["svc"]
+X_scaler = dist_pickle["scaler"]
+orient = dist_pickle["orient"]
+pix_per_cell = dist_pickle["pix_per_cell"]
+cell_per_block = dist_pickle["cell_per_block"]
+spatia = dist_pickle["spatial"]
+hist_bins = dist_pickle["hist_bins"]
+
+print('Loading is done!')
+
